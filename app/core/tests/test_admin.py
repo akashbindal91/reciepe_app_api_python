033b4e7443c7a6fb702bf6a821783d23d82bc002
Akash <akash@rupeecircle.in>
2020-07-26 22:17:43 +0530

    episode 33

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,10 +23,11 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    # def test_users_listed(self):
-    #     """Test that users are listed on the user page"""
-    #     url = reverse('admin:core_user_changelist')
-    #     res = self.client.get(url)
+    # episode 33 start
+    def test_user_page_change(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
 
-    #     self.assertContains(res, self.user.name)
-    #     self.assertContains(res, self.user.email)
+        self.assertEqual(res.status_code, 200)
+    # episode 33 ends
