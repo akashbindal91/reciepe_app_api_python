d3a7c2dbfb8791f1059ae50bf01414e372278dbe
Akash <akash@rupeecircle.in>
2020-07-31 04:06:02 +0530

    46 : create user Api updated

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -37,7 +37,12 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating a user that already exists fails"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'testpass'}
+        # payload = {'email': 'test@londonappdev.com', 'password': 'testpass'}
+        payload = {
+            'email': 'test@londonappdev.com',
+            'password': 'testpass',
+            'name': 'Test',
+        }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -45,7 +50,7 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that password must be more than 5 characters"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'pw'}
+        payload = {'email': 'test@londonappdev.com', 'password': 'aa'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
